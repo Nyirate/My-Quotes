d65c935d60a9cd4533344412fd243845bb0d99a7
Nyirate <rwakaemma34@gmail.com>
2019-08-23 17:30:56 +0200

    add a form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesFormComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
