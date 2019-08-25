e012f96e15211827c3ab26de9830322aac66c1eb
Nyirate <rwakaemma34@gmail.com>
2019-08-25 23:22:49 +0200

    add a new header

--- src/app/count-date.pipe.ts
+++ src/app/count-date.pipe.ts
@@ -6,17 +6,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CountDatePipe implements PipeTransform {
 
   transform(value: any): number {
-      let today:Date = new Date();
-      let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-      var dateDifference = Math.abs(value - todayWithNoTime) 
+      let day:Date = new Date();
+      let dayWithNoTime:any = new Date(day.getFullYear(), day.getMonth(), day.getDate())
+      var Difference = Math.abs(value - dayWithNoTime) 
       const secondsInDay = 86400; 
-      var dateDifferenceSeconds = dateDifference*0.001;
-      var dateCounter = dateDifferenceSeconds/secondsInDay;
+      var Seconds = Difference*0.001;
+      var dateCounting = Seconds/secondsInDay;
   
-      if (dateCounter >= 1 && value > todayWithNoTime){
-        return dateCounter;
+      if (dateCounting >= 1 && value > dayWithNoTime){
+        return dateCounting;
       }else{
-        return dateCounter;
+        return dateCounting;
       }
     }
 
