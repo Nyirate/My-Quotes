7793cd374517c01ece419fdeaea045e0bbccfa70
Nyirate <rwakaemma34@gmail.com>
2019-08-26 09:24:40 +0200

    add opacity

--- src/app/emma.ts
+++ src/app/emma.ts
@@ -1,6 +1,6 @@
 export class Emma {
   showDate: boolean;
-  constructor(public id: number,public name: string,public quote: string,public author: string,public newDate:Date){
+  constructor(public id: number,public name: string,public quote: string,public author: string,public publi:Date){
    this.showDate=false;
   }
 }
